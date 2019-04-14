36a07ea8836ee50a7103e692d0786854c8f2cb63
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-04-14 20:04:39 +0200

    add Redirect component

--- src/App.js
+++ src/App.js
@@ -37,6 +37,24 @@ Route.contextTypes = {
   location: PropTypes.object
 };
 
+class Redirect extends React.Component {
+
+  static contextTypes = {
+    history: PropTypes.object,
+  }
+
+  // Performs redirect to 'to' after it's mounted
+  componentDidMount() {
+    const history = this.context.history;
+    const to = this.props.to;
+    history.push(to);
+  }
+
+  render() {
+    return null;
+  }
+};
+
 class Router extends React.Component {
 
   // To expose context to children specify the type of each context
@@ -84,12 +102,18 @@ const App = () => (
             <code>/pacific</code>
           </Link>
         </li>
+        <li>
+          <Link to='/black-sea'>
+            <code>/black-sea</code>
+          </Link>
+        </li>
       </ul>
 
       <hr />
 
       <Route path='/atlantic' component={Atlantic}/>
       <Route path='/pacific' component={Pacific}/>
+      <Route path='/black-sea' component={BlackSea}/>
     </div>
   </Router>
 );
@@ -115,3 +139,35 @@ const Pacific = () => (
     </p>
   </div>
 );
+
+class BlackSea extends React.Component {
+
+  state = {
+    counter: 3
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => (
+      this.setState(prevState => {
+        return {
+          counter: prevState.counter - 1,
+        };
+      }
+    )), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Black sea</h3>
+        <p>Nothing to sea [sic] here ...</p>
+        <p>Redirecting in {this.state.counter}...</p>
+        {this.state.counter < 1 ? <Redirect to='/' /> : null}
+      </div>
+    );
+  }
+};
